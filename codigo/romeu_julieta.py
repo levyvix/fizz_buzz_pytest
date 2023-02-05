10b779a879b026357c30185032190bdd9aeda5a9
Levy Nunes <levy.vix@gmail.com>
2023-02-05 15:43:41 -0300

    romeu e julieta recebe lista

--- codigo/romeu_julieta.py
+++ codigo/romeu_julieta.py
@@ -1,11 +1,21 @@
-from typing import Union
+from typing import Union, List
 
 
-def romeu_julieta(numero: int) -> Union[str, int]:
+def romeu_julieta(numero: List[int]) -> List[Union[str, int]]:
 
-    if numero % 3 == 0:
-        return "Romeu e Julieta" if numero % 5 == 0 else "Queijo"
-    return "Goiabada" if numero % 5 == 0 else numero
+    if not isinstance(numero, list):
+        raise TypeError("O parâmetro deve ser uma lista")
+
+    res = []
+    for n in numero:
+        if not isinstance(n, int):
+            raise TypeError("Os elementos da lista devem ser inteiros")
+        if n % 3 == 0:
+            res.append("Romeu e Julieta" if n % 5 == 0 else "Queijo")
+        else:
+            res.append("Goiabada" if n % 5 == 0 else n)
+
+    return res
 
 
 if __name__ == "__main__":
