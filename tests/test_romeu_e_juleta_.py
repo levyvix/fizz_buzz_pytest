cc9e75cff990283689e55b6fbb57d929cc341a1f
Levy Nunes <levy.vix@gmail.com>
2023-02-05 15:48:00 -0300

    romeu e julieta recebe lista (#1)

--- tests/test_romeu_e_juleta_.py
+++ tests/test_romeu_e_juleta_.py
@@ -2,12 +2,12 @@ from codigo.romeu_julieta import romeu_julieta
 
 
 def test_romeu_julieta():
-    assert romeu_julieta(15) == "Romeu e Julieta"
+    assert romeu_julieta([15]) == ["Romeu e Julieta"]
 
 
 def test_romeu_juleta_multiplo_5_goiabada():
-    assert romeu_julieta(10) == "Goiabada"
+    assert romeu_julieta([10]) == ["Goiabada"]
 
 
 def test_romeu_juleta_multiplo_3_queijo():
-    assert romeu_julieta(3) == "Queijo"
+    assert romeu_julieta([3]) == ["Queijo"]
