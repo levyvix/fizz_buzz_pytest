0a0d20f02297400104f2731e1fa3cfadc1611358
Levy Nunes <levy.vix@gmail.com>
2023-02-05 16:15:05 -0300

    criacao de testes parametrizados (#2)

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,34 +1,16 @@
-import pytest
+from pytest import mark
 from codigo.fizzbuzz import fizz_buzz
 
 # given a list of numbers, return a string with the fizzbuzz result
-def test_fizz_buzz():
-    assert fizz_buzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == [
-        "1",
-        "2",
-        "Fizz",
-        "4",
-        "Buzz",
-        "Fizz",
-        "7",
-        "8",
-        "Fizz",
-        "Buzz",
-    ]
-
-
-# given a number, return a string with the fizzbuzz result
-def test_fizz_buzz_number_buzz():
-    assert fizz_buzz([10]) == ["Buzz"]
-
-
-def test_fizz_buzz_number_fizz():
-    assert fizz_buzz([9]) == ["Fizz"]
-
-
-def test_fizz_buzz_number_fizzbuzz():
-    assert fizz_buzz([15]) == ["FizzBuzz"]
-
-
-def test_fizz_buzz_number_number():
-    assert fizz_buzz([1]) == ["1"]
+@mark.parametrize(
+    "numbers, expected",
+    [
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]),
+        ([10], ["Buzz"]),
+        ([9], ["Fizz"]),
+        ([15], ["FizzBuzz"]),
+        ([1], ["1"]),
+    ],
+)
+def test_fizz_buzz(numbers, expected):
+    assert fizz_buzz(numbers) == expected
